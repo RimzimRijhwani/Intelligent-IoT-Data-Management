5c3638203ab3351c683be2c5e5904198331d4c96
Cin Sian Thang <cinsianthang@Cins-MacBook-Air.local>
2026-09-16 21:42:11 +1000

    Add countdown timer for MFA page

--- new-frontend/frontend/src/pages/ForgotPassword.jsx
+++ new-frontend/frontend/src/pages/ForgotPassword.jsx
@@ -301,10 +301,6 @@ const ForgotPassword = () => {
             </Link>
           </>
         )}
-
-
-        
-
         {message && (
           <p className="forgot-error">{message}</p>
         )}
